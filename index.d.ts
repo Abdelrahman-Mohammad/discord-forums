ef03788f0ce68f8e6fc826f1f614a37177e88a58
VukeFN <abdelrahman5abdelaal@gmail.com>
2022-07-08 12:03:43 +0200

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -3,9 +3,6 @@
 // Definitions by: Abdelrahman Mohammad <https://github.com/Abdelrahman-Mohammad/>
 // Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
 
-import { User, Message } from "discord.js";
-console.log(User)
-
 declare module "discord-forums" {
     export default class DiscordForums {
         static async connectionURL(dbURL: string): Promise<typeof import("mongoose")>;
